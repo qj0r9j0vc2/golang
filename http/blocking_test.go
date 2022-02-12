5bfdcff0f825521f0d481f88a8057f8502532248
jinu <jinwoo794533@gmail.com>
2022-02-12 10:59:35 +0900

    add TestBlocking With Timeout

--- http/blocking_test.go
+++ http/blocking_test.go
@@ -1,9 +1,12 @@
 package http
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func blocking(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +18,25 @@ func TestBlocking(t *testing.T) {
 	_, _ = http.Get(ts.URL)
 	t.Fatal("client did not block")
 
+}
+
+func TestBlockingWithTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(blocking))
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatal(err)
+		}
+		return
+	}
+
+	_ = resp.Body.Close()
 }
